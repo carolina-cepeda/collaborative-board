108426c7a563ea1d58a88cadfe37be6f582b993d
Carolina Cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2026-03-10 00:45:09 -0500

    ft: tablero inicial funcional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useEffect } from 'react';
+import * as p5lib from 'p5';
+
+const P5 = p5lib.default || p5lib;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const containerRef = useRef(null);
 
+  const sketch = function (p) {
+    p.setup = function () {
+      p.createCanvas(700, 410);
+      p.background(255);
+    };
+
+    p.draw = function () {
+      if (p.mouseIsPressed === true) {
+        p.fill(0, 0, 0);
+        p.noStroke();
+        p.ellipse(p.mouseX, p.mouseY, 20, 20);
+      }
+      if (p.mouseIsPressed === false) {
+        p.fill(255, 255, 255);
+      }
+    };
+  };
+
+  /*useEffect(() => {
+    const myp5 = new P5(sketch, containerRef.current);
+    return () => myp5.remove();
+  }, []);
+*/
+  useEffect(() => {
+    console.log('ref:', containerRef.current); // debe mostrar el div
+    console.log('P5:', P5);                    // debe mostrar la función
+    const myp5 = new P5(sketch, containerRef.current);
+    return () => myp5.remove();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '100vh',
+      backgroundColor: 200,
+      gap: 16
+    }}>
+      <h1>Tablero de Dibujo</h1>
+      <div
+        ref={containerRef}
+        style={{
+          border: '3px solid black',
+          padding: 12
+        }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
